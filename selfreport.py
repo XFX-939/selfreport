c0ce60082adeb37e333865a8659a4907875f4e85
xiangfuxing <xfx1210vip@qq.com>
2020-06-08 09:32:42 +0800

    enable linux crontab

--- selfreport.py
+++ selfreport.py
@@ -1,20 +1,30 @@
+#!/usr/bin/python3
+# coding=UTF-8
 from selenium import webdriver
+import time
 
 
 class SelfReport(object):
 
     def __init__(self):
+
         self.driver = webdriver.Chrome()
 
     def auto_report(self):
+        file_handle = open('log.txt', mode='a')
         driver = self.driver
         driver.get('https://selfreport.shu.edu.cn/Default.aspx')
         print("="*100)
+        file_handle.write('\n')
+        file_handle.write('='*100)
+        file_handle.write('\n')
         print("已进入填报网站")
+        file_handle.write(time.ctime())
+        file_handle.write('\n')
         username = driver.find_element_by_id("username")
-        username.send_keys("SHU一卡通账号")
+        username.send_keys("input your username")
         password = driver.find_element_by_id("password")
-        password.send_keys("SHU一卡通密码")
+        password.send_keys("input your password")
         print("自动填入账号密码完成")
         submit = driver.find_element_by_id("login-submit")
         submit.click()
@@ -26,11 +36,21 @@ class SelfReport(object):
         temperature.send_keys("36")
         print("填报体温完成")
         print("勾选承诺完成")
+        time.sleep(1)
         submit_res = driver.find_element_by_id("p1_ctl00_btnSubmit")
         submit_res.click()
-        driver.find_element_by_id("fineui_33").click()
+        driver.find_element_by_id("fineui_66").click()
+        print(time.ctime())
+        time.sleep(5)
+        driver.close()
         print("每日一报已完成")
         print("="*100)
+        file_handle.write('体温为36度\n')
+        file_handle.write('已完成每日一报自动填写\n')
+        file_handle.write('='*100)
+        file_handle.write('\n')
+
+        file_handle.close()
 
     def run(self):
         self.auto_report()
